61605d8ae36ff9a75bad60e4a2aa291021cace00
kotokatu <117390625+kotokatu@users.noreply.github.com>
2025-09-28 12:30:54 +0300

    feat: добавляет Error.vue

---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <NuxtLink to="/">На главную страницу</NuxtLink>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  error: {
+    type: Object,
+    default: () => {},
+  },
+});
+</script>
