900a5b8f899d65cf9963830d6b3f1cb7e7339044
kotokatu <117390625+kotokatu@users.noreply.github.com>
2025-10-01 20:27:44 +0300

    style: добавляет фон AppContactButton

--- app/components/AppContactButton.vue
+++ app/components/AppContactButton.vue
@@ -1,29 +1,15 @@
 <template>
   <div role="button" tabindex="0" class="btn">
     <svg fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        id="circlePath"
-        class="circle-path"
-        d="M56.5 104a48 48 0 1 1 0-96 48 48 0 0 1 0 96z"
-      />
+      <path id="circlePath" class="circle-path" d="M56.5 104a48 48 0 1 1 0-96 48 48 0 0 1 0 96z" />
 
       <text>
-        <textPath xlink:href="#circlePath">
-          ✦ Написать нам ✦ Написать нам ✦ Написать нам
-        </textPath>
+        <textPath xlink:href="#circlePath">✦ Написать нам ✦ Написать нам ✦ Написать нам</textPath>
       </text>
 
       <circle cx="55.5" cy="55.5" r="24" />
 
-      <svg
-        viewBox="0 0 21 18"
-        fill="none"
-        x="42"
-        y="44"
-        width="24"
-        height="24"
-        xmlns="http://www.w3.org/2000/svg"
-      >
+      <svg viewBox="0 0 21 18" fill="none" x="42" y="44" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M18.665 0.716942L0.934973 7.55394C-0.275027 8.03994 -0.268027 8.71494 0.712973 9.01594L5.26497 10.4359L15.797 3.79094C16.295 3.48794 16.75 3.65094 16.376 3.98294L7.84297 11.6839H7.84097L7.84297 11.6849L7.52897 16.3769C7.98897 16.3769 8.19197 16.1659 8.44997 15.9169L10.661 13.7669L15.26 17.1639C16.108 17.6309 16.717 17.3909 16.928 16.3789L19.947 2.15094C20.256 0.911942 19.474 0.350942 18.665 0.716942Z"
           fill="white"
@@ -36,19 +22,21 @@
 <style lang="scss" scoped>
 .btn {
   position: fixed;
-  bottom: 20px;
-  right: 20px;
-  width: 113px;
-  height: 113px;
-  border-radius: 50%;
+  bottom: 32px;
+  right: 32px;
   display: flex;
   justify-content: center;
   align-items: center;
+  width: 120px;
+  height: 120px;
+  z-index: 1000;
+  background-color: var(--q-base);
+  border-radius: 50%;
   cursor: pointer;
 
   & > svg {
-    width: 100%;
-    height: 100%;
+    width: 112px;
+    height: 112px;
 
     text {
       font-family: var(--font-secondary);
