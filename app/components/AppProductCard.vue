a16582171d5f6a691c046e2c457996f25e05b86a
Наталья Соловьева Александровна <sna@incom-sr.local>
2025-10-06 12:49:25 +0300

    style: стилизует карточки

--- app/components/AppProductCard.vue
+++ app/components/AppProductCard.vue
@@ -7,7 +7,9 @@
         <h3 class="card__price text-h3">{{ data.price }}</h3>
       </div>
 
-      <img class="card__img" :alt="data.name" :width="IMAGE_WIDTHS[size]" height="auto" :src="src" />
+      <div class="card__img-container">
+        <img class="card__img" :alt="data.name" :src="src" :width="width" />
+      </div>
 
       <ArrowIcon class="card__arrow" />
     </div>
@@ -18,7 +20,9 @@
 import ArrowIcon from "@/assets/icons/bx-arrow.svg";
 const img = useImage();
 
-const src = computed(() => img(props.data.image, { width: IMAGE_WIDTHS[props.size] }));
+const src = computed(() =>
+  img(props.data.image, { width: IMAGE_WIDTHS[props.size] })
+);
 
 const props = defineProps({
   data: {
@@ -27,9 +31,9 @@ const props = defineProps({
   },
   size: {
     type: String,
-    default: "sm",
+    default: "lg",
     validator: (value) => {
-      return ["xs", "sm", "md"].includes(value);
+      return ["sm", "md", "lg"].includes(value);
     },
   },
   hover: {
@@ -39,14 +43,17 @@ const props = defineProps({
 });
 
 const IMAGE_WIDTHS = {
-  md: 500,
-  sm: 300,
-  xs: 200,
+  lg: 500,
+  md: 300,
+  sm: 200,
 };
+
+const width = computed(() => IMAGE_WIDTHS[props.size]);
 
 const classes = computed(() => {
   return {
     hoverable: props.hover,
+    [`card--${props.size}`]: true,
   };
 });
 </script>
@@ -55,6 +62,7 @@ const classes = computed(() => {
 $transition: all 0.3s ease-in-out;
 
 .card {
+  container-type: inline-size;
   position: relative;
   width: 100%;
   height: 100%;
@@ -62,28 +70,94 @@ $transition: all 0.3s ease-in-out;
   background-color: #f8f8f8;
   border-radius: 12px;
   transition: $transition;
-}
 
-.card.hoverable {
-  @media (hover: hover) {
-    &:hover {
-      background-color: #eaeaea;
+  &--sm {
+    .card__price {
+      margin-top: auto;
+    }
+  }
 
-      .card__arrow {
-        opacity: 1;
+  &--md,
+  &--sm {
+    .card__img-container {
+      display: flex;
+      justify-content: flex-end;
+    }
+
+    .card__img {
+      object-fit: cover;
+      object-position: 35px 60px;
+    }
+  }
+
+  &--lg {
+    .card__img {
+      position: absolute;
+      top: 15%;
+      left: 40%;
+
+      transform: scale(1.1);
+
+      @container (inline-size < 600px) {
+        top: 20%;
+        left: 30%;
       }
 
-      .card__img {
-        transform: scale(1.1);
+      @container (inline-size < 450px) {
+        top: 25%;
+        left: 20%;
+      }
+
+      @container (inline-size < 400px) {
+        top: 35%;
+        left: 25%;
+      }
+
+      @container (inline-size < 350px) {
+        top: 40%;
+        transform: scale(1.3);
+      }
+    }
+  }
+
+  &.hoverable {
+    @media (hover: hover) {
+      &:hover {
+        background-color: #eaeaea;
+
+        .card__arrow {
+          opacity: 1;
+        }
+
+        .card__img {
+          transform: scale(1.2);
+        }
       }
     }
   }
 }
 
-.card__img {
+.card__img-container {
   position: absolute;
-  bottom: -30%;
-  right: -6%;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.card__img {
+  // object-fit: cover;
+  // object-fit: contain;
+
+  // position: absolute;
+  // top: 0;
+  // left: 0;
+  // width: 100%;
+  // height: auto;
+  // bottom: -10px;
+  // right: -10px;
+  // bottom: -45%;
+  // right: -20%;
   transition: $transition;
   z-index: 1;
   overflow: hidden;
@@ -93,9 +167,14 @@ $transition: all 0.3s ease-in-out;
   position: relative;
   display: flex;
   flex-direction: column;
+  height: 100%;
   gap: 16px;
   padding: 24px;
   z-index: 200;
+
+  @media (max-width: $breakpoint-sm) {
+    padding: 16px;
+  }
 }
 
 .card__title,
@@ -105,7 +184,7 @@ $transition: all 0.3s ease-in-out;
 
 .card__description,
 .card__price {
-  max-width: 50%;
+  max-width: 60%;
 }
 
 .card__price {
